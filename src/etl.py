c41a095bdabcf2d1f8aeda097867cbdd34624fee
Cole Richmond <cjrichmo@ucsd.edu>
2020-04-27 00:52:30 -0500

    add pittsburg ingestion

--- src/etl.py
+++ src/etl.py
@@ -93,6 +93,67 @@ def create_population_dataset(fname):
     pop_df = states_pop[final_cols]
     return pop_df
 
+def pitsburg(state, columns, pit_columns, inpath):
+
+     url = 'https://stacks.stanford.edu/file/druid:yg821jf8611/yg821jf8611_pa_pittsburgh_2020_04_01.csv.zip'
+
+    pits = pd.read_csv(url, nrows=500).loc[:, pit_columns]
+    pits = pits.dropna()
+    pits = pits[(pits.subject_race.isin(['white','black','asian/pacific islander','hispanic']))&(pits.officer_race != 'unknown')].reset_index(drop=True)
+
+    def group_time(time):
+        hour = int(time.split(':')[0])
+        if hour >= 6 and hour <= 11:
+            return 'Morning'
+        elif hour >= 12 and hour <=4:
+            return 'Afternoon'
+        elif hour >= 5 and hour <=8:
+            return 'Evening'
+        else:
+            return 'Night'
+
+    def officer_race_rename(race):
+        if race == 'asian/pacific islander':
+            return 'asian'
+        elif race == 'other':
+            return 'hispanic'
+        else:
+            return race
+
+
+    def bool_col(bool_):
+        if bool_:
+            return 1 
+        else:
+            return 0
+
+    def violation_filtering(violation):
+        moving_violations = ['Obedience to Traffic-Control Devices','Traffic-Control Signals',
+                             'Turning Movements and Required Signals','Careless Driving ','Speed Limits',
+                            'Turning','Speed Limitations']
+        equiptment_violations = ['Vehicle Equipment Standards','General Lighting Requirements',
+                                 'Windshield','Lighted Lamps','Registration Plate']
+        for mv in moving_violations:
+            if mv in violation:
+                return 'Moving Violation'
+        for mv in equiptment_violations:
+            if mv in violation:
+                return 'Equiptment Violation'
+        else:
+            return 'Other Violation'
+
+    filter_dict = {'time':group_time,'lat':(lambda x: round(x,2)),'lng':(lambda x: round(x,2)),'subject_race':
+                   (lambda race: 'asian' if race == 'asian/pacific islander' else race),
+                  'officer_race': officer_race_rename, 'subject_sex':(lambda sex: 1 if 'male' else 0),
+                   'officer_sex': (lambda sex: 1 if 'male' else 0),'arrest_made':bool_col,
+                   'citation_issued':bool_col,'contraband_found':bool_col,'search_conducted':bool_col,
+                  'violation':violation_filtering}
+
+    for row, func in filter_dict.items():
+        pits[row] = pits[row].apply(func)
+        
+    return pits
+
 # ---------------------------------------------------------------------
 # Driver Function(s)
 # ---------------------------------------------------------------------
@@ -119,7 +180,10 @@ def get_data(states, columns, outpath=None, inpath=None):
         else:
             inpath = None
 
-        table = get_stops(state, columns, inpath)
+        if state == "pa":
+            table = pittsburgh(state, columns, pit_columns, inpath)
+        else:
+            table = get_stops(state, columns, pit_columns, inpath)
 
         file_name = '%s_stops.csv' % (state)
         table.to_csv(os.path.join(outpath, file_name))
