8aff7a7ca9ad53b690272a977d8310924674fed1
Cole Richmond <cjrichmo@ucsd.edu>
2020-05-17 22:40:18 -0700

    add test-project

--- run.py
+++ run.py
@@ -14,6 +14,7 @@ DATA_PARAMS = 'config/data-params.json'
 CLEAN_PARAMS = 'config/process-params.json'
 TEST_DATA_PARAMS = 'config/test-data-params.json'
 TEST_CLEAN_PARAMS = 'config/test-process-params.json'
+TEST_MODEL_PARAMS = 'config/test-model.json'
 MODEL_PARAMS = 'config/model.json'
 
 def load_params(fp):
@@ -37,12 +38,15 @@ def main(targets):
         cfg = load_params(CLEAN_PARAMS)
         clean_stops(**cfg)
         
-    if 'test' in targets:
+    if 'test-project' in targets:
         cfg = load_params(TEST_DATA_PARAMS)
         get_data(**cfg)
 
         cfg = load_params(TEST_CLEAN_PARAMS)
         clean_stops(**cfg)
+
+        cfg = load_params(TEST_MODEL_PARAMS)
+        driver(**cfg)
 
     if 'model' in targets:
         cfg = load_params(MODEL_PARAMS)
